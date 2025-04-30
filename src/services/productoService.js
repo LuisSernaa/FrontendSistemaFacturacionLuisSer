67ddd6778fb0f8fdca01dc795d8adbb58067476d
LuisSernaa <fbrayan2020@gmail.com>
2025-04-30 15:10:02 -0500

    Update productoService.js

--- src/services/productoService.js
+++ src/services/productoService.js
@@ -1,7 +1,7 @@
 // productoService.js
 import axios from 'axios';
 
-const API_URL = 'https://worthy-transformation-production-b4f7.up.railway.app/api/productos';
+const API_URL = 'https://stunning-rebirth-production.up.railway.app/api/productos';
 
 export const obtenerProductos = async () => {
     return await axios.get(API_URL);
