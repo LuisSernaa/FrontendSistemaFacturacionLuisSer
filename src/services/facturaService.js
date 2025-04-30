601d2a7c4e1007ad0173aea3b4bf1365e2ee7e5d
LuisSernaa <fbrayan2020@gmail.com>
2025-04-30 15:09:35 -0500

    Update facturaService.js

--- src/services/facturaService.js
+++ src/services/facturaService.js
@@ -1,7 +1,7 @@
 // facturaService.js
 import axios from 'axios';
 
-const API_URL = 'https://worthy-transformation-production-b4f7.up.railway.app/api';
+const API_URL = 'https://stunning-rebirth-production.up.railway.app/api';
 
 export const obtenerFacturas = async () => {
     return await axios.get(`${API_URL}/facturas`);
